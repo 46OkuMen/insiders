55c8ac8892153779c5629b83aa2cc4d85e41518e
hollowaytape <hollowaytape@retro-type.com>
2018-05-29 00:50:37 -0400

    Re-encoder works; add basic map and dump of IS2 and IDS

--- codec.py
+++ codec.py
@@ -1,32 +1,33 @@
 """
-    Decompressor for Insiders '94. Currently works on the file IDS only.
+    Decompressor for Insiders '94.
 """
 import sys
-
 
 # TODO: These are magic values right now. They must come from somewhere...
 MAGIC_BX = 0x2800
 MAGIC_DX = 0x4b5a
 
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python codec.py filename")
-        sys.exit(1)
-
-    filename = sys.argv[1]
-
-    if filename == "IDS":
+def decode(filename):
+    if "IDS" in filename:
         decompression_start = MAGIC_BX
     else:
         decompression_start = 0
 
-    with open('original/%s' % filename, 'rb') as f:
+    with open(filename, 'rb') as f:
         enc = f.read()
 
-    cursor = 0
     with open('patched/%s.decompressed' % filename, 'wb') as f:
+        cursor = 0
         while cursor < decompression_start:
+            """
+                push ax
+                ax = ax XOR dx
+                pop dx
+                memory[bx] = ax
+                bx += 2
+                ax = memory[bx]
+                loop
+            """
             f.write(enc[cursor].to_bytes(length=1, byteorder='little'))
             cursor += 1
         cursor = decompression_start
@@ -36,11 +37,53 @@ if __name__ == '__main__':
         while cursor < len(enc):
             # 5a, 1e becomes 1e5a
             ax = (enc[cursor+1] << 8) + enc[cursor]
-            #print(hex(ax))
             stack.append(ax)
             ax = ax ^ dx
-            #print(hex(ax))
             dx = stack.pop()
-            #print(hex(dx))
             f.write(ax.to_bytes(length=2, byteorder='little'))
             cursor += 2
+
+def encode(filename):
+    if "IDS" in filename:
+        compression_start = MAGIC_BX
+    else:
+        compression_start = 0
+
+    with open('patched/%s.decompressed' % filename, 'rb') as f:
+        decomp = f.read()
+
+    with open('patched/%s' % filename, 'wb') as f:
+        cursor = 0
+        while cursor < compression_start:
+            f.write(decomp[cursor].to_bytes(length=1, byteorder='little'))
+            cursor += 1
+
+        cursor = compression_start
+        stack = []
+        ax = 0
+        dx = MAGIC_DX
+
+        while cursor < len(decomp):
+            ax = (decomp[cursor+1] << 8) + decomp[cursor]
+            # Oh hey, this works
+            dx = dx ^ ax
+            f.write(dx.to_bytes(length=2, byteorder='little'))
+            cursor += 2
+
+
+if __name__ == '__main__':
+    HELP_STRING = """usage: python codec.py [encode filename.decompressed] [decode filename]\n"""
+    if len(sys.argv) < 3:
+        print(HELP_STRING)
+        sys.exit(1)
+
+    mode = sys.argv[1]
+
+    filename = sys.argv[2]
+
+    if mode == 'encode':
+        encode(filename)
+    elif mode == 'decode':
+        decode(filename)
+    else:
+        print(HELP_STRING)
---
+++ dump.py
@@ -0,0 +1,131 @@
+"""
+    Generic dumper of Shift-JIS text into an excel spreadsheet.
+    Meant for quick estimations of how much text is in a game.
+"""
+
+import sys
+import os
+import xlsxwriter
+from rominfo import FILE_BLOCKS
+
+COMPILER_MESSAGES = [b'Turbo', b'Borland', b'C++', b'Library', b'Copyright']
+
+ASCII_MODE = 2
+# 0 = none
+# 1: punctuation and c format strings only (not implemented)
+# 2: All ascii
+
+THRESHOLD = 4
+
+
+def dump(files):
+    worksheet = workbook.add_worksheet('Everything')
+    worksheet.write(0, 0, 'Offset', header)
+    worksheet.write(0, 1, 'Japanese', header)
+    worksheet.write(0, 2, 'File', header)
+    worksheet.write(0, 3, 'Comments', header)
+
+    worksheet.set_column('A:A', 8)
+    worksheet.set_column('B:B', 60)
+    worksheet.set_column('C:C', 15)
+    worksheet.set_column('D:D', 60)
+    row = 1
+    for filename in files:
+        clean_filename = filename.replace('.decompressed', '')
+        blocks = FILE_BLOCKS[clean_filename]
+        with open(os.path.join('patched', filename), 'rb') as f:
+            contents = f.read()
+
+            cursor = 0
+            sjis_buffer = b""
+            sjis_buffer_start = 0
+            sjis_strings = []
+
+            for c in COMPILER_MESSAGES:
+                print(c)
+                if c in contents:
+                    #print(contents)
+                    cursor = contents.index(c)
+                    sjis_buffer_start = contents.index(c)
+                    break
+
+            while cursor < len(contents):
+                if not any([start <= cursor <= stop for (start, stop) in blocks]):
+                    cursor += 1
+                    continue
+
+                # First byte of SJIS text. Read the next one, too
+                if 0x80 <= contents[cursor] <= 0x9f or 0xe0 <= contents[cursor] <= 0xef:
+                    #print(bytes(contents[cursor]))
+                    sjis_buffer += contents[cursor].to_bytes(1, byteorder='little')
+                    cursor += 1
+                    sjis_buffer += contents[cursor].to_bytes(1, byteorder='little')
+
+                # ASCII text
+                elif 0x20 <=contents[cursor] <= 0x7e and ASCII_MODE in (1, 2):
+                    sjis_buffer += contents[cursor].to_bytes(1, byteorder='little')
+
+                # C string formatting with %
+                #elif contents[cursor] == 0x25:
+                #    #sjis_buffer += b'%'
+                #    cursor += 1
+                #    if contents[cursor]
+
+                # End of continuous SJIS string, so add the buffer to the strings and reset buffer
+                else:
+                    sjis_strings.append((sjis_buffer_start, sjis_buffer))
+                    sjis_buffer = b""
+                    sjis_buffer_start = cursor+1
+                cursor += 1
+                #print(sjis_buffer)
+
+            # Catch anything left after exiting the loop
+            if sjis_buffer:
+                sjis_strings.append((sjis_buffer_start, sjis_buffer))
+
+
+            if len(sjis_strings) == 0:
+                continue
+
+            for s in sjis_strings:
+                if len(s[1]) < THRESHOLD:
+                    continue
+
+                loc = '0x' + hex(s[0]).lstrip('0x').zfill(5)
+                try:
+                    jp = s[1].decode('shift-jis')
+                except UnicodeDecodeError:
+                    print("Couldn't decode that")
+                    continue
+
+                if len(jp.strip()) == 0:
+                    continue
+                print(loc, jp)
+
+                worksheet.write(row, 0, loc)
+                worksheet.write(row, 1, jp)
+                worksheet.write(row, 2, clean_filename)
+                row += 1
+
+    workbook.close()
+
+if __name__ == '__main__':
+    #if len(sys.argv) < 2:
+    #    print("Usage: python dumper.py folderwithgamefilesinit")
+    #    sys.exit(1)
+    #'patched' = sys.argv[1]
+    workbook = xlsxwriter.Workbook("insiders" + '_dump.xlsx')
+    header = workbook.add_format({'bold': True, 'align': 'center', 'bottom': True, 'bg_color': 'gray'})
+    #FILES = [f for f in os.listdir('patched') if os.path.isfile(os.path.join('patched', f))]
+    FILES = ['IDS.decompressed', 'IS2.decompressed']
+    print(FILES)
+    dump(FILES)
+
+
+# TODO: It'd be even better if it just took a disk, or a bunch of disks, and used ndc to get files.
+# TODO: Column for disks
+
+# TODO: Export the dump to a google doc as well?
+
+# TODO: Detect blocks and export that to a skeleton rominfo.py file.
+    # (That could help with detecting some of the one-string-only "blocks" in code sections.)
